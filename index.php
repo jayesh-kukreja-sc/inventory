459d6c340631e8ea4b78ffe8ad450ee2e61ef1a1
jayesh-kukreja-sc <49338224+jayesh-kukreja-sc@users.noreply.github.com>
2019-04-22 10:27:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,351 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MRS Mall a Entertainment Category Flat Bootstrap Responsive Website Template | Home :: w3layouts</title>
+	<!--for-mobile-apps-->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="keywords" content="MRS Mall Responsive Website Template, Web Templates, Flat Web Templates, Android Compatible web template, 
+		Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	<!--//for-mobile-apps-->
+	
+	<!-- Custom-Theme-Files -->
+    <!-- Bootstrap-CSS --> 			<link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- JQuery --> 				<script src="js/jquery.min.js"></script>
+    <!-- Bootstrap-Main --> 		<script src="js/bootstrap.min.js">		</script>
+    <!-- Index-Page-Styling --> 	<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+	<!-- Font-awesome-Styling --> 	<link rel="stylesheet" href="css/font-awesome.css" type="text/css" media="all">
+
+	
+<!-- Js for Responsive slider -->	
+	<script src="js/modernizr.js" type="text/javascript"></script>
+	<script src="js/responsiveslides.min.js"></script>
+	<script> 
+		// You can also use "$(window).load(function() {"
+		$(function () {
+		  // Slideshow 1
+		  $("#slider1").responsiveSlides({
+			 auto: true,
+			 nav: true,
+			 speed: 500,
+			 namespace: "callbacks",
+		  });
+		});
+	</script>
+
+	<!--JS for animate-->
+	<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+	<script src="js/wow.min.js"></script>
+		<script>
+			new WOW().init();
+		</script>
+	<!--//end-animate-->
+
+	<script src="js/jquery.countdown.js"></script>
+	<script src="js/script.js"></script>
+	<link rel="stylesheet" href="css/jquery.countdown.css" />
+	<link rel="stylesheet" href="css/swipebox.css">
+	
+</head>
+
+<body>
+
+<div class="header">
+
+		<div class="nav">
+			<nav class="navbar navbar-inverse navbar-fixed-top">
+				<div class="container">
+				<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						   <a class="navbar-brand" href="index.html"><img src="images/1.png" alt=""><h1>Mall</h1></a>
+						</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						
+						<ul class="nav navbar-nav navbar-right menu slide">
+							<li><a href="index.html" class="orange">Home</a></li>
+							<li><a href="about.html" class="orange">About</a></li>
+							<li><a href="shopping.html" class="orange">Shopping</a></li>
+							<li><a href="dining.html" class="orange">Dining</a></li>
+							<li><a href="entertainment.html" class="orange">Entertainment</a></li>
+							<li><a href="contact.html" class="orange">Contact</a></li>
+						</ul>
+					       
+					</div><!-- navbar-collapse -->
+				</div><!-- container -->
+			</nav>
+			<div class="clearfix"></div>
+		</div> <!-- Nav Ends -->
+		
+		<div> <!--banner Slider -->
+			<div class="cover-slider__wrap">
+				
+				<ul class="cover-slider">
+					<li class="cover-slider__slide">
+						<span class="hide">Alt Tag</span>
+					</li>
+					<li class="cover-slider__slide">
+						<span class="hide">Alt Tag</span>
+					</li>
+					<li class="cover-slider__slide">
+						<span class="hide">Alt Tag</span>
+					</li>
+					<li class="cover-slider__slide">
+						<span class="hide">Alt Tag</span>
+					</li>
+					<li class="cover-slider__slide">
+						<span class="hide">Alt Tag</span>
+					</li>
+				</ul>
+			</div>
+				<script src="js/index.js"></script>
+		</div> <!--//banner Slider -->
+</div><!--//header-->
+
+<div class="h-grid1">
+	<div class="container">
+		<div class="h-grid1-padding">
+			
+			<div class="col-md-4 h-grid1-a-w3ls">
+				<div class="h-grid1-a1">
+					<div class="h-grid1-a-icon">
+						<i> </i>
+					</div>
+					<h4> Heading - 1 </h4>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since</p>
+				</div>
+			</div>
+			<div class="col-md-4 h-grid1-b">
+				<div class="h-grid1-b1">
+					<div class="h-grid1-b-icon">
+						<i> </i>
+					</div>
+					<h4> Heading - 2 </h4>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since</p>
+				</div>
+			</div>
+			<div class="col-md-4 h-grid1-c">
+				<div class="h-grid1-c1">
+					<div class="h-grid1-c-icon">
+						<i> </i>
+					</div>
+					<h4> Heading - 3 </h4>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since</p>
+				</div>
+			</div>
+		  <div class="clearfix"> </div>
+		</div>
+	</div>
+</div><!--//h1-grid1-->	
+	
+	
+<div class="h-about">
+	<div class="h-about-padding-agile">
+	  <h2> About Mall </h2>
+		<div class="col-md-4 h-about-img">
+			<img src="./images/img10.jpg" alt="image" class="img-responsive">
+		</div>
+		<div class="col-md-4 h-about-text">
+			<h4> Heading about mall </h4>
+				<p>	Lorem Ipsum is simply dummy text of the printing and typesetting industry since	Lorem Ipsum is simply dummy text of the
+					printing and typesetting  has been the industry's standard dummy text ever since
+				</p>
+				<p>	printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since
+					Lorem Ipsum is simply dummy text. Lorem Ipsum has been the industry's standard dummy text ever since
+				</p>
+		</div>
+		<div class="col-md-4 h-about-add">
+			<div class="slider">
+				<div class="wrap">
+					<ul class="rslides" id="slider1">
+				
+						<li>
+							<div class="col-md-5 add-img-a">
+								<img src="images/add-img4.png" alt="" class="img-responsive">
+							</div>
+							<div class="col-md-7 add-info">
+								<h4> Model - 1256 </h4>
+								<h6> <label>$ </label> 254 </h6>
+								<p>Silver beet shallot wakame tomatillo salsify mung bean beetroot groundnut.</p>
+							</div> <!-- info end -->
+						  <div class="clearfix">	</div>
+						</li>
+						
+						<li>
+							<div class="add-img-b">
+								<img src="images/add-img1.png" alt="" class="img-responsive" >
+							</div>
+						</li>
+							
+						<li>
+							<div class="col-md-5 add-img-a">
+								<img src="images/add-img5.png" alt="" class="img-responsive">
+							</div>
+							<div class="col-md-7 add-info">
+								<h4> Model - 1256 </h4>
+								<h6> <label>$ </label> 254 </h6>
+								<p>Silver beet shallot wakame tomatillo salsify mung bean beetroot groundnut.</p>
+							</div> <!-- info end -->
+						  <div class="clearfix">	</div>
+						</li>
+						
+						<li>
+							<div class="add-img-b">
+								<img src="images/add-img2.png" alt="" class="img-responsive" >
+							</div>
+						</li>
+						<li>
+							<div class="col-md-5 add-img-a">
+								<img src="images/add-img6.png" alt="" class="img-responsive">
+							</div>
+							<div class="col-md-7 add-info">
+								<h4> Model - 1256 </h4>
+								<h6> <label>$ </label> 254 </h6>
+								<p>Silver beet shallot wakame tomatillo salsify mung bean beetroot groundnut.</p>
+							</div> <!-- info end -->
+						  <div class="clearfix">	</div>
+						</li>
+						
+						<li>
+							<div class="add-img-b">
+								<img src="images/add-img3.png" alt="" class="img-responsive" >
+							</div>
+						</li>
+					</ul>
+				  <div class="clearfix"></div>
+				</div> <!-- Wrap End -->
+			</div>
+		</div>
+	  <div class="clearfix"> </div>
+	</div>
+</div><!--//h-about-->	
+	
+<div class="h-grid3"><!--h-grid3-->
+	<div class="h-grid3-padding">
+	  <h2>Our Services </h2>
+			<div class="col-md-2 col-md-offset-1 h-grid3-all scroll wow zoomIn" data-wow-delay="1.5s">
+				<i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+				<h4>Heading -1</h4>
+				<p>Lorem Ipsum is text. Lorem has been Ipsum the industrys standard</p>
+			</div>
+			<div class="col-md-2 h-grid3-all scroll wow zoomIn" data-wow-delay="1s">
+				<i class="fa fa-gift" aria-hidden="true"></i>
+				<h4>Heading -2</h4>
+				<p>Lorem Ipsum is text. Lorem has been Ipsum the industrys standard</p>
+			</div>
+			<div class="col-md-2 h-grid3-all scroll wow zoomIn" data-wow-delay="0.4s">
+				<i class="fa fa-leaf" aria-hidden="true"></i>
+				<h4>Heading -3</h4>
+				<p>Lorem Ipsum is text. Lorem has been Ipsum the industrys standard</p>
+			</div>
+			<div class="col-md-2 h-grid3-all scroll wow zoomIn" data-wow-delay="1s">
+				<i class="fa fa-shopping-bag" aria-hidden="true"></i>
+				<h4>Heading -4</h4>
+				<p>Lorem Ipsum is text. Lorem has been Ipsum the industrys standard</p>
+			</div>
+			<div class="col-md-2 h-grid3-all scroll wow zoomIn" data-wow-delay="1.5s">
+				<i class="fa fa-trello" aria-hidden="true"></i>
+				<h4>Heading -5</h4>
+				<p>Lorem Ipsum is text. Lorem has been Ipsum the industrys standard</p>
+			</div>
+
+		  <div class="clearfix"> </div>
+		</div>
+</div><!--//h-grid3-->	
+	
+<div class="h-grid4"><!--h-grid4-->	
+	<div class="container">
+		<div class="h-grid4-padding-agileinfo">
+			<h3>HURRY UP!</h3>
+			<h4>Limited time offer for <span>50%</span> Discount</h4>
+			<div class="timer_wrap">
+				<div id="counter">
+					<div class="under-text">
+						
+					</div>					
+				</div>		
+			</div>
+			
+		</div> 
+	</div>
+</div><!--//h-grid4-->		
+	
+<div class="h-grid5-w3layouts"><!--h-grid5 gallery-->
+	<div class="container">
+		<div class="h-grid5-padding">
+			<h3>Gallery</h3>
+			
+			<div class="h-grid5">
+				<div class="col-md-3  h-grid5-all " >
+					<a href="images/41.jpg" class="swipebox hovereffect">  <img src="images/41.jpg" alt="image" class="hex"> </a>
+				</div>
+				<div class="col-md-3 h-grid5-all "> 
+					<a href="images/42.jpg" class="swipebox hovereffect">  <img src="images/42.jpg" alt="image" class="hex">  </a>
+				</div>
+				<div class="col-md-3 h-grid5-all">
+					<a href="images/43.jpg" class="swipebox hovereffect">  <img src="images/43.jpg" alt="image" class="hex"> </a>
+				</div>
+				<div class="col-md-3 h-grid5-all">
+					<a href="images/44.jpg" class="swipebox hovereffect">  <img src="images/44.jpg" alt="image" class="hex"> </a>
+				</div>
+			  <div class="clearfix"> </div>
+			</div>
+			
+			<div class="h-grid5">
+				<div class="col-md-3 col-md-offset-1 h-grid5-all h-grid5-m" >
+					<a href="images/45.jpg" class="swipebox hovereffect">  <img src="images/45.jpg" alt="image" class="hex"> </a>
+				</div>
+				<div class="col-md-3 h-grid5-all h-grid5-m">
+					<a href="images/46.jpg" class="swipebox hovereffect">  <img src="images/46.jpg" alt="image" class="hex"> </a>
+				</div>
+				<div class="col-md-3 h-grid5-all h-grid5-m">
+					<a href="images/47.jpg" class="swipebox hovereffect">  <img src="images/47.jpg" alt="image" class="hex"> </a>
+				</div>
+			  <div class="clearfix"> </div>
+			</div>
+			
+			<div class="h-grid5">
+				<div class="col-md-3  h-grid5-all " >
+					<a href="images/48.jpg" class="swipebox hovereffect">  <img src="images/48.jpg" alt="image" class="hex"> </a>
+				</div>
+				<div class="col-md-3 h-grid5-all ">
+					<a href="images/49.jpg" class="swipebox hovereffect">  <img src="images/49.jpg" alt="image" class="hex"> </a>
+				</div>
+				<div class="col-md-3 h-grid5-all ">
+					<a href="images/50.jpg" class="swipebox hovereffect">  <img src="images/50.jpg" alt="image" class="hex"> </a>
+				</div>
+				<div class="col-md-3 h-grid5-all ">
+					<a href="images/41.jpg" class="swipebox hovereffect">  <img src="images/41.jpg" alt="image" class="hex"> </a>
+				</div>
+			  <div class="clearfix"> </div>
+			</div>
+			
+		  <div class="clearfix"> </div>
+		</div>
+		<!-- swipe box js -->
+		<script src="js/jquery.swipebox.js"></script>
+				<script type="text/javascript">
+					;( function( $ ) {
+
+						$( '.swipebox' ).swipebox();
+
+					} )( jQuery );
+				</script>
+		<!-- //swipe box js -->
+	</div>
+</div><!--//h-grid5-->	
+	
+	
+<?php include'footer.php'?>	
+</body>
+</html>
